3043ace251f8ef5656f619267e5713cfb11d3082
Nick Clear <nickclear22@gmail.com>
2018-12-12 14:54:13 -0800

    updated twilio

--- twilio/index.js
+++ twilio/index.js
@@ -1,7 +1,5 @@
 const twilio = require('twilio');
 
-// const accountSid = 'AC7fbc45e753c45a5df8c090086bfe7cbf';
-// const authToken = '55d7e9e9aa94f0dac3e2f480d439cb1b';
 const accountSid = process.env.TWILLIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
 
